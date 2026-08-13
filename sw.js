1b71ab3341f5f6caec2e72600036d8ddd4d80da7
meiyanjie1990 <meiyanjie1990@users.noreply.github.com>
2026-08-13 11:15:34 +0800

    清空试用历史、加单张换牌按钮、背景花瓣萤火特效

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'linggan-shaizi-v10';
+const CACHE = 'linggan-shaizi-v11';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './fonts/fonts.css'];
 
 self.addEventListener('install', e => {
